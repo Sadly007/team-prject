340171477a4a636f03ddb916889306ae328e601e
KateTref <trefilovaekaterina408@gmail.com>
2022-07-16 23:23:10 +0200

    add animation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-(() => {
-const advantages__number = document.querySelector('.advantages__number');
-advantages__number.classList.remove('number-animation');
 
-// Добавить наблюдение за появлением элемента
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      advantages__number.classList.add('.number-animation');
-      return;
-    }
-    advantages__number.classList.remove('.number-animation');
-  });
-});
-    observer.observe(document.querySelector('.advantages__item'));
-})();
