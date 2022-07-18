2fffc66f4d6e5d6ea53cb784fe37e8297e329ba7
Aleksandr Akhmerov <qorbeks@gmail.com>
2022-07-18 22:49:32 +0300

    desktop and tab add| need menu fix|

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,19 +1,15 @@
+
+
 (() => {
   const refs = {
-    //  1 Кнопка
-    openModalBtn: document.querySelector('[data-modal-open]'),       
-    // 2 Кнопка
+    openModalBtn: document.querySelector('[data-modal-open]'),
     openModalBtn1: document.querySelector('[data-modal-open2]'),
-    // 3 Кнопка
-    openModalBtn2: document.querySelector('[data-modal-open3]'),
-
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.openModalBtn1.addEventListener('click', toggleModal);
-  refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
