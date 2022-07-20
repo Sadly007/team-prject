60ca72f567c00d454eefc4278cd7d89e7e8d3c80
Sadly007 <92309817+Sadly007@users.noreply.github.com>
2022-07-20 09:43:42 +0000

    Deploying to gh-pages from @ Sadly007/team-prject@4c5b020ca755042d260207147721508fe0285d7d 🚀

--- index.955ab4c3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector(".header-btn"),closeMenuBtn:document.querySelector(".close-btn"),menu:document.querySelector(".menu"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.955ab4c3.js.map
