77504fb0051d73b45c4da4fdc3681776c8aa9fb3
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-07-20 06:31:00 +0100

    addjs

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
     openModalBtn3: document.querySelector('[contacts-modal-open-one]'),
-    openModalBtn5: document.querySelector('[contacs-modal2]'),
+    openModalBtn5: document.querySelector(".button-franchise"),
     closeModalBtn3: document.querySelector('[contacts-close-one]'),
     closeModalBtn4: document.querySelector('[contacts-close3]'),
     modal3: document.querySelector('[contacts-modal-one]'),
-    modal4: document.querySelector('[contactsmodal3]'),
+    modal4: document.querySelector('[contactsmodal2]'),
 };
    refs.openModalBtn3.addEventListener('click', toggleModal3);
-  //  refs.openModalBtn5.addEventListener('click', toggleModal5);
+   refs.openModalBtn5.addEventListener('click', toggleModal5);
    refs.closeModalBtn3.addEventListener('click', toggleModal3);
    refs.closeModalBtn4.addEventListener('click', toggleModal5);
   function toggleModal3() {
